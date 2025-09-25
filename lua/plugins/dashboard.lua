fbb89d0aac5e8b8693ab01cb997451fd07925bab
JktuJQ <odmamontov@gmail.com>
2025-09-26 00:43:55 +0300

    Customized dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,117 @@
+return {
+    "nvimdev/dashboard-nvim",
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    event = "VimEnter",
+    config = function()
+        local header = {
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+            " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+            " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+            " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+            " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+            " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        }
+        local footer = {
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            [[      _ _    _              _  ____  ]],
+            [[     | | |  | |            | |/ __ \ ]],
+            [[     | | | _| |_ _   _     | | |  | |]],
+            [[ _   | | |/ / __| | | |_   | | |  | |]],
+            [[| |__| |   <| |_| |_| | |__| | |__| |]],
+            [[ \____/|_|\_\\__|\__,_|\____/ \___\_\]],
+            "",
+            "🚀 Welcome to Neovim!",
+            "🎯 Today is " .. os.date("%A, %B %d, %Y"),
+        }
+
+        vim.cmd([[highlight DashboardFooter gui=NONE cterm=NONE]])
+        require("dashboard").setup({
+            theme = "doom",
+            disable_move = false,
+            hide = {
+                statusline = false,
+                tabline = false,
+                winbar = false,
+            },
+            config = {
+                header = header,
+                center = {
+                    {
+                        icon = "󰙅 ",
+                        icon_hl = "Title",
+                        desc = "Toggle File Explorer",
+                        desc_hl = "String",
+                        key = "m",
+                        key_hl = "Number",
+                        action = "Neotree toggle"
+                    },
+                    {
+                        icon = "󰈞 ",
+                        icon_hl = "Title",
+                        desc = "Find File",
+                        desc_hl = "String",
+                        key = "f",
+                        key_hl = "Number",
+                        action = "Telescope find_files",
+                    },
+                    {
+                        icon = "󰈬 ",
+                        icon_hl = "Title",
+                        desc = "Find Word",
+                        desc_hl = "String",
+                        key = "w",
+                        key_hl = "Number",
+                        action = "Telescope live_grep",
+                    },
+                    {
+                        icon = "󰋚 ",
+                        icon_hl = "Title",
+                        desc = "Recent Files",
+                        desc_hl = "String",
+                        key = "r",
+                        key_hl = "Number",
+                        action = "Telescope oldfiles",
+                    },
+                    {
+                        icon = "󰊳 ",
+                        icon_hl = "Title",
+                        desc = "Update Plugins",
+                        desc_hl = "String",
+                        key = "u",
+                        key_hl = "Number",
+                        action = "Lazy update | MasonUpdate",
+                    },
+                    {
+                        icon = " ",
+                        icon_hl = "Title",
+                        desc = "Quit Neovim",
+                        desc_hl = "String",
+                        key = "q",
+                        key_hl = "Number",
+                        action = "qa",
+                    },
+                },
+                footer = footer
+            },
+        })
+    end,
+}

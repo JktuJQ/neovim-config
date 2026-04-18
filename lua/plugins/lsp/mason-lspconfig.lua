464139b7c31435a95663f3e5881e8127774d9144
JktuJQ <odmamontov@gmail.com>
2026-04-18 23:03:32 +0300

    lsp fix

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,22 +1,38 @@
 local opts = {
     ensure_installed = {
         "lua_ls",
-        --"pyright",
-        --"clangd",
-        --"rust_analyzer",
-        --"hls",
+        -- "pyright",
+        -- "clangd",
+        -- "rust_analyzer",
+        -- "hls",
     },
     automatic_installation = true,
 }
 
 local config = function()
     require("mason-lspconfig").setup(opts)
-end
 
-local keys = {}
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local has_blink, blink = pcall(require, "blink.cmp")
+    if has_blink then
+        capabilities = blink.get_lsp_capabilities(capabilities)
+    end
+
+    local handlers = require("plugins.lsp.nvim-lspconfig.handlers")
+
+    local servers = require("plugins.lsp.nvim-lspconfig.servers")
+    for server_name, server_config in pairs(servers) do
+        vim.lsp.config(server_name, {
+            capabilities = capabilities,
+            on_attach = handlers.on_attach,
+            settings = server_config.settings or {},
+        })
+    end
+
+    vim.lsp.enable(vim.tbl_keys(servers))
+end
 
 return {
     opts = opts,
     config = config,
-    keys = keys,
 }
--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,32 +1,7 @@
-local servers = require("plugins.lsp.nvim-lspconfig.servers")
 local handlers = require("plugins.lsp.nvim-lspconfig.handlers")
-
-local opts = {
-    capabilities = nil,
-    on_attach = handlers.on_attach,
-    servers = servers,
-}
 
 local config = function()
     handlers.setup_diagnostics()
-
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-    local has_blink, blink = pcall(require, "blink.cmp")
-    if has_blink then
-        capabilities = blink.get_lsp_capabilities(capabilities)
-    end
-
-    for server_name, server_config in pairs(servers) do
-        local final_config = vim.tbl_deep_extend("force", {
-            capabilities = capabilities,
-            on_attach = handlers.on_attach,
-        }, server_config)
-
-        vim.lsp.config(server_name, final_config)
-    end
-
-    vim.lsp.enable(vim.tbl_keys(servers))
 
     vim.api.nvim_create_autocmd("LspAttach", {
         group = handlers.augroup,
@@ -39,10 +14,6 @@ local config = function()
     })
 end
 
-local keys = {}
-
 return {
-    opts = opts,
     config = config,
-    keys = keys,
 }
